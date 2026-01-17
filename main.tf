e3f1e03844511e541fd19399d355626f9f586e29
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-17 14:23:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,8 +81,8 @@ resource "helm_release" "tf_ssd" {
 
   wait            = true
   cleanup_on_fail = true
-  atomic          = false
   timeout         = 1800 
+  atomic  = false   # prevent automatic uninstall on failure
 
   lifecycle {
     prevent_destroy = true
