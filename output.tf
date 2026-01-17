d3cb1b80f5d86a72f82f06cc942f3b90d42c69ff
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-17 13:56:32 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-# Output Helm Release Names
+# Output Helm Release Name
 output "helm_release_name" {
-  value = [for r in helm_release.tf_ssd : r.name]
+  value = helm_release.tf_ssd.name
 }
 
-
+# Output Helm Namespace
 output "helm_namespace" {
   value = var.namespace
 }
@@ -12,5 +12,3 @@ output "helm_namespace" {
 output "ssd_ingress_host" {
   value = var.ingress_hosts
 }
-
-
